e85bf73c509c74efbf87a648fa37f5cb14fa1cb0
rjrodger <richard@ricebridge.com>
2019-03-18 15:05:32 +0000

    first

---
+++ voxgig.js
@@ -0,0 +1,34 @@
+/* Copyright (c) 2019 voxgig and other contributors, MIT License */
+'use strict'
+
+
+const Wreck = require('wreck')
+
+module.exports = make_voxgig
+
+
+function make_voxgig(options) {
+  return new Voxgig(options)
+}
+
+make_voxgig.search = async function(query) {
+  return new Voxgig().search(query)
+}
+
+function Voxgig(options) {
+  var self = this
+
+  self.search = async function(query) {
+    const url = 'https://www.voxgig.com/api/search'
+
+    const msg = {
+      role:'web',
+      app:'search',
+      cmd:'query',
+      query:query
+    }
+
+    const { res, payload } = await Wreck.post(url,{payload:msg})
+    return JSON.parse(payload.toString())
+  }
+}

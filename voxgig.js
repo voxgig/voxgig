4d61d71e606a96244565e5926eb15d47c86c1cae
rjrodger <richard@ricebridge.com>
2019-03-18 15:09:09 +0000

    v0.0.1

--- voxgig.js
+++ voxgig.js
@@ -1,34 +1,32 @@
 /* Copyright (c) 2019 voxgig and other contributors, MIT License */
-'use strict'
+"use strict";
 
+const Wreck = require("wreck");
 
-const Wreck = require('wreck')
-
-module.exports = make_voxgig
-
+module.exports = make_voxgig;
 
 function make_voxgig(options) {
-  return new Voxgig(options)
+  return new Voxgig(options);
 }
 
 make_voxgig.search = async function(query) {
-  return new Voxgig().search(query)
-}
+  return new Voxgig().search(query);
+};
 
 function Voxgig(options) {
-  var self = this
+  var self = this;
 
   self.search = async function(query) {
-    const url = 'https://www.voxgig.com/api/search'
+    const url = "https://www.voxgig.com/api/search";
 
     const msg = {
-      role:'web',
-      app:'search',
-      cmd:'query',
-      query:query
-    }
+      role: "web",
+      app: "search",
+      cmd: "query",
+      query: query
+    };
 
-    const { res, payload } = await Wreck.post(url,{payload:msg})
-    return JSON.parse(payload.toString())
-  }
+    const { res, payload } = await Wreck.post(url, { payload: msg });
+    return JSON.parse(payload.toString());
+  };
 }
